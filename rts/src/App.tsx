952172853dec1f8364317f4d6d7cfa327c54f485
Aleksandar Ristic <storiaca@gmail.com>
2022-04-20 09:51:54 +0200

    change app

--- rts/src/App.tsx
+++ rts/src/App.tsx
@@ -5,17 +5,7 @@ const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>App<h1/>
       </header>
     </div>
   );
