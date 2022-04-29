463de8e2884254dd460001324e8b520de8c6a32e
Aleksandar Ristic <storiaca@gmail.com>
2022-04-29 08:52:38 +0200

    Handling drag events too

--- rts/src/events/EventComponent.tsx
+++ rts/src/events/EventComponent.tsx
@@ -5,9 +5,16 @@ const EventComponent: FC = () => {
     console.log(event);
   };
 
+  const handleDragStart = (event: React.DragEvent<HTMLDivElement>) => {
+    console.log(event);
+  };
+
   return (
     <div>
       <input onChange={handlerChange} />
+      <div draggable onDragStart={handleDragStart}>
+        Drag Me
+      </div>
     </div>
   );
 };
