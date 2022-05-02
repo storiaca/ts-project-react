227fd05c582df54b75d17d6348424ce0d4e3fc8c
Aleksandar Ristic <storiaca@gmail.com>
2022-05-02 12:06:16 +0200

    Redux project setup

---
+++ redux-ts/src/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+// import { createRoot } from "react-dom/client";
+import App from "./App";
+
+import "./index.css";
+
+// let root = createRoot(document.getElementById("root") as HTMLElement);
+// root.render(
+//   <React.StrictMode>
+//     <UserSearch />
+//     <EventComponent />
+//     {/* <GuestList /> */}
+//   </React.StrictMode>
+// );
+
+ReactDOM.render(<App />, document.getElementById("root"));
