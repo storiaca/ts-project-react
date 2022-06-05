fa1c5cb32fb58afb7c55d0d4c3d0b81bdff35eca
Aleksandar Ristic <storiaca@gmail.com>
2022-06-05 16:44:41 +0200

    Wiring Up to React

---
+++ redux-ts/src/components/RepositoriesList.tsx
@@ -0,0 +1,14 @@
+import React, { FC } from "react";
+
+const RepositoriesList: FC = () => {
+  return (
+    <div>
+      <form>
+        <input />
+        <button>Search</button>
+      </form>
+    </div>
+  );
+};
+
+export default RepositoriesList;
