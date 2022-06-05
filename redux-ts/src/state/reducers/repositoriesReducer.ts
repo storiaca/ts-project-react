ad00f164545924d99678dd979c96d587d411ef6b
Aleksandar Ristic <storiaca@gmail.com>
2022-06-05 16:48:33 +0200

    Setup initial state

--- redux-ts/src/state/reducers/repositoriesReducer.ts
+++ redux-ts/src/state/reducers/repositoriesReducer.ts
@@ -7,20 +7,26 @@ interface RepositoriesState {
   data: string[];
 }
 
+const initialState = {
+  loading: false,
+  error: null,
+  data: [],
+};
+
 const reducer = (
-  state: RepositoriesState, 
+  state: RepositoriesState = initialState,
   action: Action
-):RepositoriesState  => {
+): RepositoriesState => {
   switch (action.type) {
     case ActionType.SEARCH_REPOSITORIES:
-      return {loading: true, error: null, data: []};
+      return { loading: true, error: null, data: [] };
     case ActionType.SEARCH_REPOSITORIES_SUCCESS:
-      return {loading: false, error: null, data: action.payload};
+      return { loading: false, error: null, data: action.payload };
     case ActionType.SEARCH_REPOSITORIES_ERROR:
-      return {loading: false, error: action.payload, data: []};
+      return { loading: false, error: action.payload, data: [] };
     default:
       return state;
   }
-}
+};
 
 export default reducer;
