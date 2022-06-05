09232c63edbf6d0cc715658bd0151a1c3fad14e4
Aleksandar Ristic <storiaca@gmail.com>
2022-06-05 17:15:50 +0200

    Calling an action creatior

--- redux-ts/src/components/RepositoriesList.tsx
+++ redux-ts/src/components/RepositoriesList.tsx
@@ -1,10 +1,14 @@
 import React, { FC, useState } from "react";
+import { useDispatch } from "react-redux";
+import { actionCreators } from "../state";
 
 const RepositoriesList: FC = () => {
   const [term, setTerm] = useState("");
+  const dispatch = useDispatch();
 
   const termSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+    dispatch(actionCreators.searchRepositories(term));
   };
 
   return (
