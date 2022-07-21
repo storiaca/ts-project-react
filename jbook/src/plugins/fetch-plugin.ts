8460c1ef748cd7d9f95cdefd301e361406c44fa3
Aleksandar Ristic <storiaca@gmail.com>
2022-07-21 09:04:44 +0200

    Configuring the correct loader for css

--- jbook/src/plugins/fetch-plugin.ts
+++ jbook/src/plugins/fetch-plugin.ts
@@ -31,8 +31,10 @@ export const fetchPlugin = (inputCode: string) => {
         }
         const { data, request } = await axios.get(args.path);
 
+        const loader = args.path.match(/.css$/) ? "css" : "jsx";
+
         const result: esbuild.OnLoadResult = {
-          loader: "jsx",
+          loader: loader,
           contents: data,
           resolveDir: new URL("./", request.responseURL).pathname,
         };
