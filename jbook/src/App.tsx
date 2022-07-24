0b49999bb98cdc21187c198b83b8472077b9e158
Aleksandar Ristic <storiaca@gmail.com>
2022-07-24 15:08:21 +0200

    Sandboxins an iframe

--- jbook/src/App.tsx
+++ jbook/src/App.tsx
@@ -55,7 +55,7 @@ function App() {
         <button onClick={onClick}>Submit</button>
       </div>
       <pre>{code}</pre>
-      <iframe src="/test.html" />
+      <iframe sandbox="allow-same-origin" src="/test.html" />
     </div>
   );
 }
