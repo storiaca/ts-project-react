11c4a9e54330f886f22f9e26fbf166bce6172f21
Aleksandar Ristic <storiaca@gmail.com>
2022-09-29 08:54:07 +0200

    A few more options

--- jbook/src/components/CodeEditor.tsx
+++ jbook/src/components/CodeEditor.tsx
@@ -7,8 +7,14 @@ const CodeEditor = () => {
       height="500px"
       options={{
         wordWrap: "on",
+        minimap: { enabled: false },
+        showUnused: false,
+        folding: false,
+        lineNumbersMinChars: 3,
+        fontSize: 16,
+        scrollBeyondLastLine: false,
+        automaticLayout: true,
       }}
-      // need to setup editor for js
     />
   );
 };
