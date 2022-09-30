56c2a581f790c1d6fcdd088d7d33f47103d1f39e
Aleksandar Ristic <storiaca@gmail.com>
2022-09-30 08:14:58 +0200

    Adding a type defeinition

--- jbook/src/components/CodeEditor.tsx
+++ jbook/src/components/CodeEditor.tsx
@@ -1,5 +1,5 @@
-import React, { FC } from "react";
-import MonacoEditor from "@monaco-editor/react";
+import { FC } from "react";
+import MonacoEditor, { EditorDidMount } from "@monaco-editor/react";
 
 interface CodeEditorProps {
   initialValue: string;
@@ -7,10 +7,12 @@ interface CodeEditorProps {
 }
 
 const CodeEditor: FC<CodeEditorProps> = ({ initialValue, onChange }) => {
-  const onEditorDidMount = (getValue: () => string, monacoEditor: any) => {
+  const onEditorDidMount: EditorDidMount = (getValue, monacoEditor) => {
     monacoEditor.onDidChangeModelContent(() => {
       onChange(getValue());
     });
+
+    monacoEditor.getModel()?.updateOptions({ tabSize: 2 });
   };
 
   return (
