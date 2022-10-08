a4b0cda884f4e5ee515cfb2dbec6d13508aaf094
Aleksandar Ristic <storiaca@gmail.com>
2022-10-08 12:06:58 +0200

    Setting a resize handle

--- jbook/src/components/Resizable.tsx
+++ jbook/src/components/Resizable.tsx
@@ -1,12 +1,17 @@
 import React, { FC } from "react";
 import { ResizableBox } from "react-resizable";
+import "./resizable.css";
 
 interface ResizableProps {
   direction: "horizontal" | "vertical";
   children?: React.ReactNode;
 }
 const Resizable: FC<ResizableProps> = ({ direction, children }) => {
-  return <div>{children}</div>;
+  return (
+    <ResizableBox height={300} width={300} resizeHandles={["s"]}>
+      {children}
+    </ResizableBox>
+  );
 };
 
 export default Resizable;
