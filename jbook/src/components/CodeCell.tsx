5373e97606d184a5a94b09b635274df89846de86
Aleksandar Ristic <storiaca@gmail.com>
2022-10-12 08:31:41 +0200

    Resizing horizontally

--- jbook/src/components/CodeCell.tsx
+++ jbook/src/components/CodeCell.tsx
@@ -15,10 +15,12 @@ const CodeCell = () => {
   return (
     <Resizable direction="vertical">
       <div style={{ height: "100%", display: "flex" }}>
-        <CodeEditor
-          initialValue="const a = 1;"
-          onChange={(value) => setInput(value)}
-        />
+        <Resizable direction="horizontal">
+          <CodeEditor
+            initialValue="const a = 1;"
+            onChange={(value) => setInput(value)}
+          />
+        </Resizable>
         <Preview code={code} />
       </div>
     </Resizable>
