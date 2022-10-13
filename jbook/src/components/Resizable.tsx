8c33c3c98894bebe4309ebe0abbbd223a75d8655
Aleksandar Ristic <storiaca@gmail.com>
2022-10-13 12:44:21 +0200

    Applying horizontal resize props

--- jbook/src/components/Resizable.tsx
+++ jbook/src/components/Resizable.tsx
@@ -10,21 +10,23 @@ const Resizable: FC<ResizableProps> = ({ direction, children }) => {
   let resizableProps: ResizableBoxProps;
 
   if (direction === "horizontal") {
-    resizableProps = {};
+    resizableProps = {
+      minConstraints: [window.innerWidth * 0.2, Infinity],
+      maxConstraints: [window.innerWidth * 0.75, Infinity],
+      height: Infinity,
+      width: window.innerWidth * 0.75,
+      resizeHandles: ["e"],
+    };
   } else {
-    resizableProps = {};
+    resizableProps = {
+      minConstraints: [Infinity, 24],
+      maxConstraints: [Infinity, window.innerHeight * 0.9],
+      height: 300,
+      width: Infinity,
+      resizeHandles: ["s"],
+    };
   }
-  return (
-    <ResizableBox
-      minConstraints={[Infinity, 24]}
-      maxConstraints={[Infinity, window.innerHeight * 0.9]}
-      height={300}
-      width={Infinity}
-      resizeHandles={["s"]}
-    >
-      {children}
-    </ResizableBox>
-  );
+  return <ResizableBox {...resizableProps}>{children}</ResizableBox>;
 };
 
 export default Resizable;
