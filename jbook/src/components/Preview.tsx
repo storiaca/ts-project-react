29afa407b392140c6bfb1936745f2d1a129c27cb
Aleksandar Ristic <storiaca@gmail.com>
2022-10-17 08:29:57 +0200

    adding and execution timeout

--- jbook/src/components/Preview.tsx
+++ jbook/src/components/Preview.tsx
@@ -31,7 +31,9 @@ const Preview: FC<PreviewProps> = ({ code }) => {
 
   useEffect(() => {
     iframe.current.srcdoc = html;
-    iframe.current.contentWindow.postMessage(code, "*");
+    setTimeout(() => {
+      iframe.current.contentWindow.postMessage(code, "*");
+    }, 50);
   }, [code]);
 
   return (
