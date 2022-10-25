738c9f4e5aff0d7ada12f2122e7ab656e39a5a92
Aleksandar Ristic <storiaca@gmail.com>
2022-10-25 08:02:35 +0200

    Adding state to the editor

--- jbook/src/components/TextEditor.tsx
+++ jbook/src/components/TextEditor.tsx
@@ -4,6 +4,7 @@ import "./text-editor.css";
 const TextEditor: FC = () => {
   const ref = useRef<HTMLDivElement | null>(null);
   const [editing, setEditing] = useState(false);
+  const [value, setValue] = useState("# Header");
 
   useEffect(() => {
     const listener = (event: MouseEvent) => {
@@ -27,13 +28,13 @@ const TextEditor: FC = () => {
   if (editing) {
     return (
       <div className="text-editor" ref={ref}>
-        <MDEditor />
+        <MDEditor value={value} onChange={(v) => setValue(v || "")} />
       </div>
     );
   }
   return (
     <div className="text-editor" onClick={() => setEditing(true)}>
-      <MDEditor.Markdown source={"# Header"} />
+      <MDEditor.Markdown source={value} />
     </div>
   );
 };
