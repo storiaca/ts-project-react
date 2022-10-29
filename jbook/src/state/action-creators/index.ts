92da5b216ba13a8b50dbd4075a08edb2dd2c4f61
Aleksandar Ristic <storiaca@gmail.com>
2022-10-29 11:22:03 +0200

    Action creator return type annotation

--- jbook/src/state/action-creators/index.ts
+++ jbook/src/state/action-creators/index.ts
@@ -1,1 +1,16 @@
-export {};
+import { ActionType } from "../action-types";
+import {
+  Action,
+  UpdateCellAction,
+  DeleteCellAction,
+  MoveCellAction,
+  InsertCellBeforeAction,
+} from "../actions";
+
+export const updateCell = (): UpdateCellAction => {};
+
+export const deleteCell = (): DeleteCellAction => {};
+
+export const moveCell = (): MoveCellAction => {};
+
+export const insertCellCell = (): InsertCellBeforeAction => {};
