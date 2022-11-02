7f1d275096c016804980e50907ed681d7ede45e1
Aleksandar Ristic <storiaca@gmail.com>
2022-11-02 08:45:14 +0100

    Delete code for testing

--- jbook/src/state/store.ts
+++ jbook/src/state/store.ts
@@ -2,26 +2,5 @@ import { createStore, applyMiddleware } from "redux";
 //import { configureStore } from "@reduxjs/toolkit";
 import thunk from "redux-thunk";
 import reducers from "./reducers";
-import { ActionType } from "./action-types";
 
 export const store = createStore(reducers, {}, applyMiddleware(thunk));
-
-store.dispatch({
-  type: ActionType.INSERT_CELL_BEFORE,
-  payload: {
-    id: null,
-    type: "code",
-  },
-});
-
-store.dispatch({
-  type: ActionType.INSERT_CELL_BEFORE,
-  payload: {
-    id: null,
-    type: "text",
-  },
-});
-
-//const id = store.getState().cells.order[0];
-
-console.log(store.getState());
