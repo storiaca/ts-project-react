151fedb8bfdec99fcaf5502b0439153f61bb9a76
Aleksandar Ristic <storiaca@gmail.com>
2022-11-03 08:51:21 +0100

    Creating a typed selector

--- jbook/src/components/CellList.tsx
+++ jbook/src/components/CellList.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
-
+import { usedTypedSelector } from "../hooks/usedTypedSelector";
 const CellList: FC = () => {
+  usedTypedSelector((state) => state);
   return <div>CellList</div>;
 };
 
