b8aa3295764cda77a0608d0cf9703c57c309ebd6
Aleksandar Ristic <storiaca@gmail.com>
2022-11-21 08:55:00 +0100

    Forcing add cell visibility

--- jbook/src/components/AddCell.tsx
+++ jbook/src/components/AddCell.tsx
@@ -4,13 +4,14 @@ import { useActions } from "../hooks/useActions";
 
 interface AddCellProps {
   nextCellId: string | null;
+  forceVisible?: boolean;
 }
 
-const AddCell: FC<AddCellProps> = ({ nextCellId }) => {
+const AddCell: FC<AddCellProps> = ({ nextCellId, forceVisible }) => {
   const { insertCellBefore } = useActions();
 
   return (
-    <div className="add-cell">
+    <div className={`add-cell ${forceVisible && "force-visible"}`}>
       <div className="add-buttons">
         <button
           className="button is-rounded is-primary is-small"
@@ -21,8 +22,14 @@ const AddCell: FC<AddCellProps> = ({ nextCellId }) => {
           </span>
           <span>Code</span>
         </button>
-        <button onClick={() => insertCellBefore(nextCellId, "text")}>
-          Text
+        <button
+          className="button is-rounded is-primary is-small"
+          onClick={() => insertCellBefore(nextCellId, "text")}
+        >
+          <span className="icon is-small">
+            <i className="fas fa-plus"></i>
+          </span>
+          <span>Text</span>
         </button>
       </div>
       <div className="divider"></div>
