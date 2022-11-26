450438f2b6a4693c411980be9f1729c608c20844
Aleksandar Ristic <storiaca@gmail.com>
2022-11-26 13:19:48 +0100

    Add redux to comoponents

--- jbook/src/components/CellList.tsx
+++ jbook/src/components/CellList.tsx
@@ -10,14 +10,14 @@ const CellList: FC = () => {
 
   const renderedCells = cells.map((cell) => (
     <Fragment key={cell.id}>
-      <AddCell nextCellId={cell.id} />
       <CellListItem cell={cell} />
+      <AddCell previousCellId={cell.id} />
     </Fragment>
   ));
   return (
     <div>
+      <AddCell forceVisible={cells.length === 0} previousCellId={null} />
       {renderedCells}
-      <AddCell forceVisible={cells.length === 0} nextCellId={null} />
     </div>
   );
 };
