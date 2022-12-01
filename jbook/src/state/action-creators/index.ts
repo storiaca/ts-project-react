d81cbe74e3035498ad42645b10d420b43e29985b
Aleksandar Ristic <storiaca@gmail.com>
2022-12-01 23:18:07 +0100

    Create bundle action creator

--- jbook/src/state/action-creators/index.ts
+++ jbook/src/state/action-creators/index.ts
@@ -1,3 +1,4 @@
+import { Dispatch } from "redux";
 import { ActionType } from "../action-types";
 import {
   Action,
@@ -5,9 +6,12 @@ import {
   DeleteCellAction,
   MoveCellAction,
   InsertCellAfterAction,
+  BundleStartAction,
+  BundleCompleteAction,
   Direction,
 } from "../actions";
 import { CellTypes } from "../cell";
+import bundle from "../../bundler";
 
 export const updateCell = (id: string, content: string): UpdateCellAction => {
   return {
@@ -46,5 +50,29 @@ export const insertCellAfter = (
       id,
       type: cellType,
     },
+  };
+};
+
+export const createBundle = (cellId: string, input: string) => {
+  return async (dispatch: Dispatch<Action>) => {
+    dispatch({
+      type: ActionType.BUNDLE_START,
+      payload: {
+        cellId,
+      },
+    });
+
+    const result = await bundle(input);
+
+    dispatch({
+      type: ActionType.BUNDLE_COMPLETE,
+      payload: {
+        cellId,
+        bundle: {
+          code: result.code,
+          err: result.err,
+        },
+      },
+    });
   };
 };
