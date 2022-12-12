7cd5b59743552b805677bde3281dbee0ade51f3f
Aleksandar Ristic <storiaca@gmail.com>
2022-12-12 21:50:33 +0100

    Fixing the background

--- jbook/src/components/CodeCell.tsx
+++ jbook/src/components/CodeCell.tsx
@@ -39,15 +39,17 @@ const CodeCell: FC<CodeCellProps> = ({ cell }) => {
             onChange={(value) => updateCell(cell.id, value)}
           />
         </Resizable>
-        {!bundle || bundle.loading ? (
-          <div className="progress-cover">
-            <progress className="progress is-small is-primary" max="100">
-              Loading
-            </progress>
-          </div>
-        ) : (
-          <Preview code={bundle.code} bundlingStatus={bundle.err} />
-        )}
+        <div className="progress-wrapper">
+          {!bundle || bundle.loading ? (
+            <div className="progress-cover">
+              <progress className="progress is-small is-primary" max="100">
+                Loading
+              </progress>
+            </div>
+          ) : (
+            <Preview code={bundle.code} bundlingStatus={bundle.err} />
+          )}
+        </div>
       </div>
     </Resizable>
   );
