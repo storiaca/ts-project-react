2140e451c8af8e730a5b92b91c2852fc7d0fca5c
Aleksandar Ristic <storiaca@gmail.com>
2022-12-20 22:08:00 +0100

    Create cumulative code

--- jbook/src/components/CodeCell.tsx
+++ jbook/src/components/CodeCell.tsx
@@ -18,7 +18,13 @@ const CodeCell: FC<CodeCellProps> = ({ cell }) => {
     const { data, order } = state.cells;
     const orderedCells = order.map((id) => data[id]);
 
-    const cumulativeCode = [];
+    const cumulativeCode = [
+      `
+        const show = (value) => {
+          document.querySelector('#root').innerHTML = value;
+        }
+      `,
+    ];
 
     for (let c of orderedCells) {
       if (c.type === "code") {
@@ -31,22 +37,20 @@ const CodeCell: FC<CodeCellProps> = ({ cell }) => {
     return cumulativeCode;
   });
 
-  console.log(cumulativeCode);
-
   useEffect(() => {
     if (!bundle) {
-      createBundle(cell.id, cell.content);
+      createBundle(cell.id, cumulativeCode.join("\n"));
       return;
     }
     const timer = setTimeout(async () => {
-      createBundle(cell.id, cell.content);
+      createBundle(cell.id, cumulativeCode.join("\n"));
     }, 750);
 
     return () => {
       clearTimeout(timer);
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [cell.content, cell.id, createBundle]);
+  }, [cumulativeCode.join("\n"), cell.id, createBundle]);
 
   return (
     <Resizable direction="vertical">
